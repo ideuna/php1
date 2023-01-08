7ddcc899f5d81398d40284610416c473680efed4
ideuna <idol75@gmail.com>
2023-01-08 16:18:16 +0900

    회원수정제작, 회원탈퇴와 함께 넣음

--- lib/top.php
+++ lib/top.php
@@ -97,9 +97,8 @@
                                     <div class="login-register-btn mr-50">
                                         <?php if(isset($_SESSION['id'])) { ?>
                                         <a
-                                            href="#"><?php if(isset($_SESSION['name'])) { echo $_SESSION['name']; } ?></a>
+                                            href="user.php"><?php if(isset($_SESSION['name'])) { echo $_SESSION['name']; } ?></a>
                                         <a href="_logout.php"> / Logout</a>
-                                        <a href="_signout.php"> / 회원탈퇴</a>
                                         <?php } else { ?>
                                         <a href="login.php" id="loginBtn">Login</a>
                                         <a href="join.php"> / Register </a>
